4865c490d4a4008246ba5010216b8dd821f26bfb
kouki135 <arumaziki2000@gmail.com>
2022-02-08 15:44:51 +0900

    テストコード記述完了

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {'亜意卯'}
+    first_name            {'絵尾'}
+    last_name_kana        {'アイウ'}
+    first_name_kana       {'エオ'}
+    birthday              {Faker::Date.between(from: '1930-01-01', to: '2017-12-31')}
+  end
+end

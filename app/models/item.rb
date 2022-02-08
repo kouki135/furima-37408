242d38eb0412a97c547c342df1f5747dcfa3549a
kouki135 <arumaziki2000@gmail.com>
2022-02-08 23:08:28 +0900

    itemモデルへバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
-  validates :nickname, presence: true
-  
+  validates :product_name, presence: true
+  validates :product_explanation, presence: true
+  validates :product_category_id, presence: true
+  validates :product_condition_id, presence: true
+  validates :delivery_charge_side_id, presence: true
+  validates :delivery_source_area_id, presence: true
+  validates :delivery_days_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
   belongs_to :user
 end

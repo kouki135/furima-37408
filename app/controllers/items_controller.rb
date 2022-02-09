0a0357b409a04b7d8e9e55990344e494d7a5b4bd
kouki135 <arumaziki2000@gmail.com>
2022-02-09 17:50:35 +0900

    出品した商品のテーブル保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, onry: [:new]
+before_action :authenticate_user!, only: [:new]
 
   def index
     
@@ -10,8 +10,17 @@ before_action :authenticate_user!, onry: [:new]
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :product_explanation, :product_category_id, :product_condition_id, :delivery_charge_side_id, :delivery_source_area_id, :delivery_days_id, :price, :image).merge(user_id: current_user.id)
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   validates :delivery_source_area_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_days_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, numericality: { in: 300..9999999}, format: /\d/
-  validates :image, presence: true,
+  validates :image, presence: true
 
   belongs_to :user
   has_one_attached :image
